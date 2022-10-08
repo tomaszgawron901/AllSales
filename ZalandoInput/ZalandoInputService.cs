00f06e5a001429c18a62506affb90e2869dcdca9
tomaszgawron901 <mrgawron901@gmail.com>
2022-10-08 21:42:42 +0200

    create ZalandoInputService base

---
+++ ZalandoInput/ZalandoInputService.cs
@@ -0,0 +1,12 @@
+﻿using AllSales.Shared.Models;
+using AllSales.Shared.Services;
+
+namespace ZalandoInput;
+
+public class ZalandoInputService : IInputService
+{
+    public Task<List<Product>> GetSaleProducts()
+    {
+        throw new NotImplementedException();
+    }
+}
